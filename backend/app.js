f54b2a32eeaee6015ca157a414eb8ce17a37b951
Hossein <hosseinyadegarnia1391@gmail.com>
2025-08-01 23:48:04 +0330

    Add login basic codes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import {auth_router} from "./routes/auth.js";
 import { passwords_router } from "./routes/passwords.js";
 import {database_sequelize} from "./database/database.js"
+import cors from "cors"
 
 let app = express()
+
+app.use(cors())
 
 app.use("/auth" , auth_router)
 app.use("/passwords" , passwords_router)
 
-database_sequelize.sync({alter: true})
-app.listen(4000)
+database_sequelize.sync()
+app.listen(4001)
